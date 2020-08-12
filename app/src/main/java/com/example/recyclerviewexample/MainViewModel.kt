3b326f52d6011d146d2d99e15067799ebb6b96e1
SongYungi <syg1138@gmail.com>
2020-08-12 18:19:38 +0900

    [Default] fix bug

--- app/src/main/java/com/example/recyclerviewexample/MainViewModel.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.recyclerviewexample
 
-import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.recyclerviewexample.vo.TestInfoVo
@@ -11,15 +10,16 @@ class MainViewModel : ViewModel() {
     val testList =  MutableLiveData<List<TestInfoVo>>()
 
     fun updateList() {
-        tempTestList.add(TestInfoVo("홍길동1","010-1111-1111"))
-        tempTestList.add(TestInfoVo("홍길동2","010-2222-2222"))
-        tempTestList.add(TestInfoVo("홍길동3","010-3333-3333"))
+        tempTestList.clear()
+        tempTestList.add(TestInfoVo("People1","010-1111-1111"))
+        tempTestList.add(TestInfoVo("People2","010-2222-2222"))
+        tempTestList.add(TestInfoVo("People3","010-3333-3333"))
         testList.postValue(tempTestList)
     }
 
     var number : Int = 4
     fun addButtonClick() {
-        tempTestList.add(TestInfoVo("홍길동${number}","010-9999-9999"))
+        tempTestList.add(TestInfoVo("People${number}","010-9999-9999"))
         number++
         testList.postValue(tempTestList)
     }
